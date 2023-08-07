b0384fd8bc444ca74a56e5f40a4d9be770526494
huongtx <phamhyta@gmail.com>
2023-08-07 11:51:43 +0700

    fix css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
     <div>
       <div className='bg-[url("https://noahbogo.com/static/media/LoginPageImage.1b66fae6.jpg")] h-[600px] bg-no-repeat w-full bg-cover'>
         <div className="flex justify-center items-center h-full w-full text-white">
-          <div className="w-1/3 bg-[#1e1e1e] rounded-xl shadow-xl px-10">
+          <div className="w-1/3 bg-[#1e1e1e] rounded-xl shadow-xl px-10 ">
             <h1 className="text-4xl font-semibold my-6 text-center">NOAH</h1>
             <div className="text-xl font-bold my-4">Sign in</div>
             <div>
@@ -18,7 +18,7 @@ const Login = () => {
                 type="email"
                 name="email"
                 id="email"
-                className="w-full p-2 rounded-md outline-none"
+                className="w-full p-2 rounded-md outline-none text-black"
               />
             </div>
             <div className="my-6">
@@ -31,7 +31,7 @@ const Login = () => {
                 type="password"
                 name="password"
                 id="password"
-                className="w-full p-2 rounded-md outline-none"
+                className="w-full p-2 rounded-md outline-none text-black"
               />
             </div>
             <button className="bg-orange-500 rounded-lg w-full p-3 mt-4 text-center shadow-xl text-xl font-medium">
